0f3b027d70e5821fbac13555c1b574b2e5005e22
YouSafe <yelagabani@hotmail.com>
2023-07-01 09:07:53 +0200

    Add parsing for the last move of position command

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,33 @@ enum StartingPosition {
     Custom(/* Todo: fen*/)
 }
 
+#[derive(Debug)]
 struct Position {
     rank: u8,
     file: u8
 }
 
+#[derive(Debug)]
+enum Promotion {
+    Queen,
+    Bishop,
+    Rook,
+    Knight,
+}
+
+// TODO: pack move data tighter
+#[derive(Debug)]
 struct Move {
     from: Position,
     to: Position,
+    promotion: Option<Promotion>
 }
 
 enum Command {
     Uci,
     IsReady,
     NewGame,
-    Position(StartingPosition, Vec<Move>),
+    Position(StartingPosition, Option<Move>),
     Go,
     Stop,
     Quit
@@ -39,7 +51,30 @@ impl EngineUCI {
             "uci" => Some(Command::Uci),
             "isready" => Some(Command::IsReady),
             "ucinewgame" => Some(Command::NewGame),
-            "position" => Some(Command::Position(StartingPosition::Standard, vec![])),
+            "position" => {
+                let starting_pos = match parts.next() {
+                    Some("startpos") => StartingPosition::Standard,
+                    None => {
+                        eprintln!("position command is not valid: {message}");
+                        return;
+                    },
+                    _ => todo!("FEN starting position not supported yet")
+                };
+
+                let last_move = match parts.next() {
+                    Some("moves") => self.parse_last_move(parts.collect()),
+                    Some(_) => {
+                        eprintln!("position command does not include `moves` keyword");
+                        None
+                    },
+                    _ => {
+                        eprintln!("position command is not valid: {message}");
+                        return;
+                    },
+                };
+
+                Some(Command::Position(starting_pos, last_move))
+            },
             "go" => Some(Command::Go),
             "stop" => Some(Command::Stop),
             "quit" => Some(Command::Quit),
@@ -64,8 +99,8 @@ impl EngineUCI {
             Command::NewGame => {
 
             }
-            Command::Position(start_pos, moves) => {
-
+            Command::Position(start_pos, last_move) => {
+                eprintln!("DEBUG: last_move: {:?}", last_move);
             }
             Command::Go => {
                 println!("bestmove d2d4");
@@ -78,4 +113,57 @@ impl EngineUCI {
             }
         }
     }
+
+    fn parse_last_move(&self, move_strings: Vec<&str>) -> Option<Move> {
+        let last_move_string = move_strings.last();
+
+        // TODO: program should not panic if an invalid move is parsed
+        last_move_string.map(
+            |move_str | self.parse_move(move_str).expect("invalid move")
+        )
+
+    }
+
+    fn parse_move(&self, move_str: &str) -> Result<Move, &'static str> {
+        if move_str.len() < 4 || move_str.len() > 5 {
+            return Err("Invalid move format");
+        }
+
+        let chars: Vec<char> = move_str.chars().collect();
+
+        let from_file = chars[0];
+        let from_rank = chars[1];
+        let to_file = chars[2];
+        let to_rank = chars[3];
+
+        // TODO: validate positions
+        let from = Position {
+            file: (from_file as u8) - ('a' as u8) + 1,
+            rank: (from_rank as u8) - ('1' as u8) + 1,
+        };
+
+        let to = Position {
+            file: (to_file as u8) - ('a' as u8) + 1,
+            rank: (to_rank as u8) - ('1' as u8) + 1,
+        };
+
+        let promotion = if move_str.len() == 5 {
+            match chars[4] {
+                'q' => Some(Promotion::Queen),
+                'b' => Some(Promotion::Bishop),
+                'r' => Some(Promotion::Rook),
+                'n' => Some(Promotion::Knight),
+                _ => return Err("Invalid promotion"),
+            }
+        } else {
+            None
+        };
+
+        Ok(Move {
+            from,
+            to,
+            promotion,
+        })
+    }
+
 }
