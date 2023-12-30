eda7b6d45fd8039646780c73ce1dcde570afe6e4
YouSafe <yelagabani@hotmail.com>
2023-12-30 21:37:13 +0100

    Add script for publishing

---
+++ www/utils/publish.js
@@ -0,0 +1,10 @@
+/*
+ * Add the following to your package.json
+ * "deploy": "vite build --base=/chess-engine/ && node utils/publish.js"
+ */
+import { publish } from "gh-pages";
+
+publish("dist", { history: false, dotfiles: true }, (err) => {
+  if (err) console.error(err);
+  else console.log("Published to GitHub");
+});
