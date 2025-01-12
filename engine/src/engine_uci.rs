3be48cad3c27d56db1d02e7df72ad88d50e77030
YouSafe <yelagabani@hotmail.com>
2025-01-12 10:26:50 +0100

    Refactor engine_uci

--- engine/src/engine_uci.rs
+++ engine/src/engine_uci.rs
@@ -3,8 +3,14 @@ use crate::search_limits::{SearchLimits, TimeLimit};
 use crate::types::color::Color;
 use crate::uci_move::UCIMove;
 use crate::{Printer, SearchWorkerPool};
-use std::str::FromStr;
+use std::str::{FromStr, Split};
 use std::time::Duration;
+
+pub struct EngineUCI<S: SearchWorkerPool, P: Printer> {
+    board: Board,
+    workers: S,
+    printer: P,
+}
 
 #[derive(Debug, PartialEq)]
 enum Command {
@@ -41,11 +47,6 @@ enum StartingPosition {
     Custom(Board),
 }
 
-pub struct EngineUCI<S: SearchWorkerPool, P: Printer> {
-    board: Board,
-    workers: S,
-    printer: P,
-}
 
 impl<S: SearchWorkerPool, P: Printer> EngineUCI<S, P> {
     pub fn new(workers: S, printer: P) -> Self {
@@ -72,120 +73,8 @@ impl<S: SearchWorkerPool, P: Printer> EngineUCI<S, P> {
             "uci" => Command::Uci,
             "isready" => Command::IsReady,
             "ucinewgame" => Command::NewGame,
-            "position" => {
-                let token = parts.next();
-                let starting_pos;
-                match token {
-                    Some("startpos") => {
-                        starting_pos = StartingPosition::Standard;
-                        parts.next(); // consume "moves" token
-                    }
-                    Some("fen") => {
-                        // this also consumes the "moves" token
-                        let fen = parts
-                            .by_ref()
-                            .take_while(|s| *s != "moves")
-                            .collect::<Vec<_>>()
-                            .join(" ");
-
-                        starting_pos = StartingPosition::Custom(
-                            Board::from_str(fen.as_str())
-                                .map_err(|_| ParseCommandError::InvalidStartingPos)?,
-                        );
-                    }
-                    _ => return Err(ParseCommandError::MissingParts),
-                }
-
-                let moves = parts
-                    .map(UCIMove::from_str)
-                    .collect::<Result<Vec<_>, _>>()
-                    .map_err(|_| ParseCommandError::InvalidMove)?;
-
-                Command::Position(starting_pos, moves)
-            }
-            "go" => {
-                let mut depth: Option<u8> = None;
-                let mut mate: Option<u8> = None;
-                let mut time_left: [Duration; 2] = Default::default();
-                let mut move_time: Option<Duration> = None;
-                let mut increment: [Duration; 2] = Default::default();
-                let mut moves_to_go: Option<u8> = None;
-                let mut nodes: Option<u64> = None;
-                let mut infinite = false;
-
-                while let Some(token) = parts.next() {
-                    match token {
-                        "infinite" => {
-                            infinite = true;
-                        }
-                        "wtime" | "btime" | "winc" | "binc" | "movetime" => {
-                            let param = parts
-                                .next()
-                                .ok_or(ParseCommandError::MissingParts)?
-                                .parse()
-                                .map_err(|_| ParseCommandError::InvalidNumber)?;
-
-                            let param = Duration::from_millis(param);
-
-                            match token {
-                                "wtime" => time_left[Color::White as usize] = param,
-                                "btime" => time_left[Color::Black as usize] = param,
-                                "winc" => increment[Color::White as usize] = param,
-                                "binc" => increment[Color::Black as usize] = param,
-                                "movetime" => move_time = Some(param),
-                                _ => unreachable!(),
-                            }
-                        }
-                        "depth" | "mate" | "moves_to_go" => {
-                            let param = parts
-                                .next()
-                                .ok_or(ParseCommandError::MissingParts)?
-                                .parse()
-                                .map_err(|_| ParseCommandError::InvalidNumber)?;
-
-                            match token {
-                                "depth" => depth = Some(param),
-                                "mate" => mate = Some(param),
-                                "moves_to_go" => moves_to_go = Some(param),
-                                _ => unreachable!(),
-                            }
-                        }
-                        "nodes" => {
-                            let param = parts
-                                .next()
-                                .ok_or(ParseCommandError::MissingParts)?
-                                .parse()
-                                .map_err(|_| ParseCommandError::InvalidNumber)?;
-
-                            nodes = Some(param);
-                        }
-                        _ => {}
-                    }
-                }
-
-                let time = if infinite {
-                    TimeLimit::Infinite
-                } else if let Some(move_time) = move_time {
-                    TimeLimit::Fixed { move_time }
-                } else if !time_left.contains(&Duration::default()) {
-                    TimeLimit::Dynamic {
-                        time_left,
-                        increment,
-                    }
-                } else {
-                    TimeLimit::Infinite
-                };
-
-                let limits = SearchLimits {
-                    time,
-                    depth,
-                    mate,
-                    nodes,
-                    moves_to_go,
-                };
-
-                Command::Go(limits)
-            }
+            "position" => parse_position(&mut parts)?,
+            "go" => parse_go(parts)?,
             "debug" => Command::Debug,
             "eval" => Command::Eval,
             "stop" => Command::Stop,
@@ -238,4 +127,113 @@ impl<S: SearchWorkerPool, P: Printer> EngineUCI<S, P> {
             }
         }
     }
+}
+
+fn parse_go(mut parts: Split<'_, char>) -> Result<Command, ParseCommandError> {
+    let mut depth: Option<u8> = None;
+    let mut mate: Option<u8> = None;
+    let mut time_left: [Duration; 2] = Default::default();
+    let mut move_time: Option<Duration> = None;
+    let mut increment: [Duration; 2] = Default::default();
+    let mut moves_to_go: Option<u8> = None;
+    let mut nodes: Option<u64> = None;
+    let mut infinite = false;
+    while let Some(token) = parts.next() {
+        match token {
+            "infinite" => {
+                infinite = true;
+            }
+            "wtime" | "btime" | "winc" | "binc" | "movetime" => {
+                let param = parts
+                    .next()
+                    .ok_or(ParseCommandError::MissingParts)?
+                    .parse()
+                    .map_err(|_| ParseCommandError::InvalidNumber)?;
+
+                let param = Duration::from_millis(param);
+
+                match token {
+                    "wtime" => time_left[Color::White as usize] = param,
+                    "btime" => time_left[Color::Black as usize] = param,
+                    "winc" => increment[Color::White as usize] = param,
+                    "binc" => increment[Color::Black as usize] = param,
+                    "movetime" => move_time = Some(param),
+                    _ => unreachable!(),
+                }
+            }
+            "depth" | "mate" | "moves_to_go" => {
+                let param = parts
+                    .next()
+                    .ok_or(ParseCommandError::MissingParts)?
+                    .parse()
+                    .map_err(|_| ParseCommandError::InvalidNumber)?;
+
+                match token {
+                    "depth" => depth = Some(param),
+                    "mate" => mate = Some(param),
+                    "moves_to_go" => moves_to_go = Some(param),
+                    _ => unreachable!(),
+                }
+            }
+            "nodes" => {
+                let param = parts
+                    .next()
+                    .ok_or(ParseCommandError::MissingParts)?
+                    .parse()
+                    .map_err(|_| ParseCommandError::InvalidNumber)?;
+
+                nodes = Some(param);
+            }
+            _ => {}
+        }
+    }
+    let time = if infinite {
+        TimeLimit::Infinite
+    } else if let Some(move_time) = move_time {
+        TimeLimit::Fixed { move_time }
+    } else if !time_left.contains(&Duration::default()) {
+        TimeLimit::Dynamic {
+            time_left,
+            increment,
+        }
+    } else {
+        TimeLimit::Infinite
+    };
+    let limits = SearchLimits {
+        time,
+        depth,
+        mate,
+        nodes,
+        moves_to_go,
+    };
+    Ok(Command::Go(limits))
+}
+
+fn parse_position(parts: &mut Split<'_, char>) -> Result<Command, ParseCommandError> {
+    let token = parts.next();
+    let starting_pos;
+    match token {
+        Some("startpos") => {
+            starting_pos = StartingPosition::Standard;
+            parts.next(); // consume "moves" token
+        }
+        Some("fen") => {
+            // this also consumes the "moves" token
+            let fen = parts
+                .by_ref()
+                .take_while(|s| *s != "moves")
+                .collect::<Vec<_>>()
+                .join(" ");
+
+            starting_pos = StartingPosition::Custom(
+                Board::from_str(fen.as_str()).map_err(|_| ParseCommandError::InvalidStartingPos)?,
+            );
+        }
+        _ => return Err(ParseCommandError::MissingParts),
+    }
+    let moves = parts
+        .map(UCIMove::from_str)
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|_| ParseCommandError::InvalidMove)?;
+    Ok(Command::Position(starting_pos, moves))
 }
